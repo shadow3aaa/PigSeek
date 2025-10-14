c05e76eede062928c57abf08203d840c84dd734e
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2025-10-14 21:45:48 +0800

    chore: v1.1.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -70,7 +70,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1"
     }
     packaging {
         resources {
@@ -98,7 +98,7 @@ compose.desktop {
 
         nativeDistributions {
             packageName = "PigSeek"
-            packageVersion = "1.0.0"
+            packageVersion = "1.1.0"
 
             targetFormats(TargetFormat.Msi, TargetFormat.Dmg, TargetFormat.Deb)
 
